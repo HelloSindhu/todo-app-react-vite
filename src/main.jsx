56b0da5649dc177ed7ba69997e214f8cf94fe312
HelloSindhu <63118530+HelloSindhu@users.noreply.github.com>
2023-11-07 14:04:06 +0530

    Setting up Project

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+import App from './App.jsx'
+import './index.css'
+
+const theme = extendTheme({
+  fonts: {
+    body: "'Josefin Sans',sans-serif",
+  }
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
